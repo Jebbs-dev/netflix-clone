896caa4624bfbfd2c3c397d09a407bdface9aed0
Jebbs-dev <ojebiyifulness@gmail.com>
2023-12-07 23:38:37 +0100

    feat: add logic for login and signup
    Added features and authentication for signup and signin

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,20 +1,57 @@
 "use client";
 
+import axios from "axios";
 import { Input } from "@/components/Input";
 import React, { useCallback, useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 const Auth = () => {
+  const router = useRouter();
+
   const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
 
   const [variant, setVariant] = useState("login");
 
   const toggleVariant = useCallback(() => {
-    setVariant(
-      currentVariant => currentVariant === "login" ? "register" : "login"
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
     );
   }, []);
+
+  const login = useCallback(async () => {
+    try {
+      await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: "/",
+      });
+
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        email,
+        name,
+        password,
+      });
+
+      login();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password, login]);
 
   return (
     <div className="relative w-full h-full min-h-screen bg-hero bg-no-repeat bg-center bg-fixed bg-cover">
@@ -30,39 +67,54 @@ const Auth = () => {
             <div className="flex flex-col gap-4">
               {variant === "register" && (
                 <Input
-                  id="username"
-                  type="text"
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                    setUsername(e.target.value);
-                  }}
-                  value={username}
                   label="Username"
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    setName(e.target.value);
+                  }}
+                  id="name"
+                  value={name}
                 />
               )}
               <Input
-                id="email"
-                type="email"
+                label="Email"
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                   setEmail(e.target.value);
                 }}
+                id="email"
+                type="email"
                 value={email}
-                label="Email"
               />
               <Input
-                id="password"
-                type="password"
+                label="Password"
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                   setPassword(e.target.value);
                 }}
+                id="password"
+                type="password"
                 value={password}
-                label="Password"
               />
             </div>
             <button
+              onClick={variant === "login" ? login : register}
               className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
             >
               {variant === "login" ? "Login" : "Sign up"}
             </button>
+
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div
+                onClick={() => signIn("google", { callbackUrl: "/" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FcGoogle size={30} />
+              </div>
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FaGithub size={30} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant === "login"
                 ? "First time using Netflix?"
