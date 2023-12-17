9235e55d34499758ee185d0a4f43a5a5c1bdb867
Jebbs-dev <ojebiyifulness@gmail.com>
2023-12-17 18:36:52 +0100

    feat: add homepage components
    Added the homepage components such as navbar, infomodal, billboard, movielist

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,11 @@
+import Billboard from "@/components/Billboard";
+import InfoModal from "@/components/InfoModal";
+import MovieList from "@/components/MovieList";
+import Navbar from "@/components/Navbar";
 import useCurrentUser from "@/hooks/useCurrentUser";
+import useFavorites from "@/hooks/useFavorites";
+import useInfoModal from "@/hooks/useInfoModal";
+import useMovieList from "@/hooks/useMovieList";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
 
@@ -20,15 +27,19 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser()
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModal();
 
   return (
     <main>
-      <h1 className="text-4xl text-green-500">Netflix Clone</h1>
-      <p className="text-white">Logged in as: {user?.name}</p>
-      <button onClick={() => signOut()} className="h-10 w-full bg-white">
-        Logout
-      </button>
+      <InfoModal visible={isOpen} onClose={closeModal} />
+      <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList data={movies} title="Trending Now" />
+        <MovieList data={favorites} title="My List" />
+      </div>
     </main>
   );
 }
